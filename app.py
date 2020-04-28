94fccc6774caa1106ab3fbbb96bdcb71eed7f923
Natalia <nataliia.sherbakova@gmail.com>
2020-04-28 23:31:55 +0300

    search by first name added

--- app.py
+++ app.py
@@ -1,31 +1,37 @@
 # importing flask module
-from flask import Flask, render_template, request, redirect, url_for, Markup, flash, session
+from typing import List, Any
 
+from flask import Flask, render_template, request, redirect, url_for, Markup, flash, session
+import secrets
 # from flask_login import logout_user
 
 import urllib.request, urllib.parse, urllib.error
 import re
+import json
 
 app = Flask(__name__, static_url_path='/static')
-
 
 @app.route('/')
 def index():
     if session.keys().__contains__('user_id'):
         return home()
+    if session.get("friends") is None:
+        session["friends"] = []
 
     return render_template('button.html')
 
+def get_friends_list(user_id, token):
+    friends_url = get_friends_url(user_id, token)
+    response = connect(friends_url)
+    parsed = json.loads(response)
+    return parsed["response"]
 
 def home():
     user_id = session["user_id"]
     token = session["token"]
 
-    friends_url = get_friends_url(user_id, token)
-    response = connect(friends_url)
-    parsed = eval(response)
-    # print(parsed)
-    n_friends = parsed['response']['count']
+    friends = get_friends_list(user_id, token)
+    n_friends = friends['count']
 
     album_url = get_album_url(user_id, "profile", 1, 1, token)
     connection = urllib.request.urlopen(album_url)
@@ -34,9 +40,12 @@ def home():
     photo_json = eval(data)
 
     user_pic_url = re.sub('//////', '//', re.sub('\\\/', '///', photo_json['response']['items'][0]['sizes'][0]['url']))
+    session["pic_url"] = user_pic_url
+    session["n_friends"] = n_friends
 
-    print(user_pic_url)
-    return render_template('friends.html', pic=user_pic_url, friends=n_friends)
+
+
+    return render_template('friends.html', pic=session["pic_url"], friends=session["n_friends"], friend_list = session["friends"])
 
 
 @app.route('/login')
@@ -44,7 +53,7 @@ def login():
     return render_template('login.html')
 
 
-app.secret_key = '_5#y2L"F64Q8z\n\xec]/'
+app.secret_key =  secrets.token_hex(16)
 app_id = 7414616
 redirect_uri = 'http://127.0.0.1:5000/o/oauth2/auth'
 client_secret = '6kMClHK2fRQXJSHM0rHr'
@@ -76,6 +85,8 @@ def button():
 @app.route('/logout_button', methods=["GET", "POST"])
 def logout_button():
     session.clear()
+    #app.config["SECRET_KEY"] =  secrets.token_hex(16)
+
     return index()
 
 
@@ -99,6 +110,19 @@ def get_code():
 
     return redirect("/")
 
+@app.route('/search', methods = ["GET", "POST"] )
+def submit(name="unknown"):
+    friends_info = get_friends_list(session["user_id"],session["token"])["items"]
+    items = [(item["first_name"]+" "+item["last_name"]) for item in friends_info]
+    session["friends"] = items
+    if request.method == "POST":
+        name = request.form.get("friend")
+        print("name"+name)
+        session["friends"] = [item["first_name"]+" "+item["last_name"] for item in friends_info if item["first_name"] == name]
+
+
+    return render_template('friends.html',pic=session["pic_url"], friends=session["n_friends"], friend_list = session["friends"], friend_name=name)
+
 
 def get_access_token_url(client_id, client_secret, redirect_uri, code):
     base = 'https://oauth.vk.com/access_token?'
@@ -114,8 +138,9 @@ def get_friends_url(user_id, token):
     base = 'https://api.vk.com/method/friends.get?PARAMETERS'
     token = '&access_token=' + token
     user_id = '&user_id=' + str(user_id)
+    fields = '&fields=city'
     version_string = "&v=5.103"
-    return base + user_id + token + version_string
+    return base + user_id + fields+token + version_string
 
 
 def get_album_url(owner_id, album_id, chronological, maxN, access_token):
@@ -130,6 +155,7 @@ def get_album_url(owner_id, album_id, chronological, maxN, access_token):
     version_string = "&v=5.103"
     url = base + owner_string + album_string + rev_string + count_string + photo_sizes_string + access_token_string + version_string
     return url
+
 
 
 if __name__ == "__main__":
