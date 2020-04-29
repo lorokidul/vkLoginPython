47d3ea0bc6368e50e014358a85b1b54249b5e2f6
Natalia <nataliia.sherbakova@gmail.com>
2020-04-30 00:21:04 +0300

    Information about search results added

--- app.py
+++ app.py
@@ -1,30 +1,33 @@
 # importing flask module
-from typing import List, Any
 
-from flask import Flask, render_template, request, redirect, url_for, Markup, flash, session
+import json
+import re
 import secrets
+import urllib.error
+import urllib.parse
+import urllib.request
+
+from flask import Flask, render_template, request, redirect, session
+
 # from flask_login import logout_user
 
-import urllib.request, urllib.parse, urllib.error
-import re
-import json
-
 app = Flask(__name__, static_url_path='/static')
+
 
 @app.route('/')
 def index():
     if session.keys().__contains__('user_id'):
         return home()
-    if session.get("friends") is None:
-        session["friends"] = []
 
     return render_template('button.html')
+
 
 def get_friends_list(user_id, token):
     friends_url = get_friends_url(user_id, token)
     response = connect(friends_url)
     parsed = json.loads(response)
     return parsed["response"]
+
 
 def home():
     user_id = session["user_id"]
@@ -38,22 +41,20 @@ def home():
     data = connection.read().decode()
 
     photo_json = eval(data)
+    session["pic_url"] = None
+    if len(photo_json['response']['items']) > 0:
+        user_pic_url = re.sub('//////', '//',
+                              re.sub('\\\/', '///', photo_json['response']['items'][0]['sizes'][0]['url']))
+        session["pic_url"] = user_pic_url
 
-    user_pic_url = re.sub('//////', '//', re.sub('\\\/', '///', photo_json['response']['items'][0]['sizes'][0]['url']))
-    session["pic_url"] = user_pic_url
     session["n_friends"] = n_friends
+    session["queries"] = 0
+
+    return render_template('friends.html', pic=session["pic_url"], friends=session["n_friends"],
+                           query_counter=session["queries"])
 
 
-
-    return render_template('friends.html', pic=session["pic_url"], friends=session["n_friends"], friend_list = session["friends"])
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
-app.secret_key =  secrets.token_hex(16)
+app.secret_key = secrets.token_hex(16)
 app_id = 7414616
 redirect_uri = 'http://127.0.0.1:5000/o/oauth2/auth'
 client_secret = '6kMClHK2fRQXJSHM0rHr'
@@ -63,7 +64,6 @@ def get_url(client_id, redirect_uri):
     base = "https://oauth.vk.com/authorize?"
     client_id = 'client_id=' + str(client_id)
     redirect_uri = '&redirect_uri=' + str(redirect_uri)
-    response = '&response=code'
     scope = '&scope=friends'
     version_string = "&v=5.103"
     url = base + client_id + redirect_uri + scope + version_string + "&revoke=1"
@@ -85,8 +85,8 @@ def button():
 @app.route('/logout_button', methods=["GET", "POST"])
 def logout_button():
     session.clear()
-    #app.config["SECRET_KEY"] =  secrets.token_hex(16)
-
+    session.pop('token', None)
+    session.pop('user_id', None)
     return index()
 
 
@@ -107,21 +107,22 @@ def get_code():
         session.clear()
         redirect("/")
 
-
     return redirect("/")
 
-@app.route('/search', methods = ["GET", "POST"] )
+
+@app.route('/search', methods=["GET", "POST"])
 def submit(name="unknown"):
-    friends_info = get_friends_list(session["user_id"],session["token"])["items"]
-    items = [(item["first_name"]+" "+item["last_name"]) for item in friends_info]
+    friends_info = get_friends_list(session["user_id"], session["token"])["items"]
+    items = [(item["first_name"] + " " + item["last_name"]) for item in friends_info]
     session["friends"] = items
     if request.method == "POST":
         name = request.form.get("friend")
-        print("name"+name)
-        session["friends"] = [item["first_name"]+" "+item["last_name"] for item in friends_info if item["first_name"] == name]
+        session["friends"] = [item["first_name"] + " " + item["last_name"] for item in friends_info if
+                              item["first_name"] == name]
+        session["queries"] += 1
 
-
-    return render_template('friends.html',pic=session["pic_url"], friends=session["n_friends"], friend_list = session["friends"], friend_name=name)
+    return render_template('friends.html', pic=session["pic_url"], friends=session["n_friends"],
+                           friend_list=session["friends"], friend_name=name, query_counter=session["queries"])
 
 
 def get_access_token_url(client_id, client_secret, redirect_uri, code):
@@ -140,7 +141,7 @@ def get_friends_url(user_id, token):
     user_id = '&user_id=' + str(user_id)
     fields = '&fields=city'
     version_string = "&v=5.103"
-    return base + user_id + fields+token + version_string
+    return base + user_id + fields + token + version_string
 
 
 def get_album_url(owner_id, album_id, chronological, maxN, access_token):
@@ -151,11 +152,9 @@ def get_album_url(owner_id, album_id, chronological, maxN, access_token):
     count_string = '&count=' + str(maxN)
     access_token_string = '&access_token=' + str(access_token)
     photo_sizes_string = "&photo_sizes=0"
-    extended_string = "&extended=0"
     version_string = "&v=5.103"
     url = base + owner_string + album_string + rev_string + count_string + photo_sizes_string + access_token_string + version_string
     return url
-
 
 
 if __name__ == "__main__":
