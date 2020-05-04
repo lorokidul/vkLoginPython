d0edc8fa8c522e121e5415eca14314979b85daf1
Natalia <nataliia.sherbakova@gmail.com>
2020-05-04 16:35:30 +0300

    urllib lib  replaced with requests

--- app.py
+++ app.py
@@ -1,12 +1,5 @@
-
-
-import json
 import re
 import secrets
-import urllib.error
-import urllib.parse
-import urllib.request
-
 import requests
 from flask import Flask, render_template, request, redirect, session
 
@@ -19,7 +12,6 @@ CLIENT_SECRET = '6kMClHK2fRQXJSHM0rHr'
 API_VERSION = '5.103'
 
 
-
 @app.route('/')
 def index():
     if 'user_id' in session.keys():
@@ -29,10 +21,13 @@ def index():
 
 
 def get_friends_list(user_id, token):
-    friends_url = get_friends_url(user_id, token)
-    response = connect(friends_url)
-    parsed = json.loads(response)
-    return parsed["response"]
+    response = requests.get('https://api.vk.com/method/friends.get',
+                             params={'access_token':token,
+                                     'user_id':user_id,
+                                     'fields':'city',
+                                     'v':API_VERSION})
+    response_json = response.json()
+    return response_json["response"]
 
 
 def home():
@@ -63,26 +58,14 @@ def home():
 
 
 
-def get_url(client_id, redirect_uri):
-    base = "https://oauth.vk.com/authorize?"
-    client_id = 'client_id=' + str(client_id)
-    redirect_uri = '&redirect_uri=' + str(redirect_uri)
-    scope = '&scope=friends'
-    version_string = "&v=5.103"
-    url = base + client_id + redirect_uri + scope + version_string + "&revoke=1"
-    return url
-
-
-def connect(url):
-    connection = urllib.request.urlopen(url)
-    data = connection.read().decode()
-    return data
-
-
 @app.route('/login_button', methods=["GET", "POST"])
 def button():
-    url = get_url(APP_ID, REDIRECT_URI)
-    return redirect(url)
+    r = requests.get('https://oauth.vk.com/authorize',
+                 params={'client_id':APP_ID,
+                         'redirect_uri':REDIRECT_URI,
+                         'scope':'friends',
+                         'v':API_VERSION})
+    return redirect(r.url)
 
 
 @app.route('/logout_button', methods=["GET", "POST"])
@@ -97,12 +80,16 @@ def logout_button():
 def get_code():
     try:
         code = request.args.get('code')
-        token_url = get_access_token_url(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
-        response = connect(token_url)
-        parsed = json.loads(response)
 
-        token = parsed['access_token']
-        user_id = parsed['user_id']
+        response = requests.get('https://oauth.vk.com/access_token',
+                                params={'client_id':APP_ID,
+                                        'client_secret':CLIENT_SECRET,
+                                        'redirect_uri':REDIRECT_URI,
+                                        'code':code})
+
+        response_json = response.json()
+        token = response_json['access_token']
+        user_id = response_json['user_id']
 
         session['user_id'] = user_id
         session['token'] = token
@@ -126,26 +113,6 @@ def submit(name="unknown"):
 
     return render_template('friends.html', pic=session["pic_url"], friends=session["n_friends"],
                            friend_list=session["friends"], friend_name=name, query_counter=session["queries"])
-
-
-def get_access_token_url(client_id, client_secret, redirect_uri, code):
-    base = 'https://oauth.vk.com/access_token?'
-    client_id = 'client_id=' + str(client_id)
-    client_secret = '&client_secret=' + str(client_secret)
-    redirect_uri = '&redirect_uri=' + str(redirect_uri)
-    code = '&code=' + code
-
-    return base + client_id + client_secret + redirect_uri + code
-
-
-def get_friends_url(user_id, token):
-    base = 'https://api.vk.com/method/friends.get?PARAMETERS'
-    token = '&access_token=' + token
-    user_id = '&user_id=' + str(user_id)
-    fields = '&fields=city'
-    version_string = "&v=5.103"
-    return base + user_id + fields + token + version_string
-
 
 if __name__ == "__main__":
     app.run()
